130e88dfe95df5327af6ad6267c2c25c7ad2df6c
yhonda-ohishi <m.tama.ramu@gmail.com>
2025-07-16 15:38:53 +0900

    interpriter

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -3,6 +3,8 @@ module.exports = {
     apps: [{
         name: 'bun-test-app',
         script: 'dist/index.js',
+        interpreter: 'bun', // <-- PM2にBunを使用するように指示するためにこの行を追加
+
         // このパスが正しく、絶対パスであることを確認してください
         cwd: 'C:/actions-runner/_work/bun-test/bun-test/', // <-- これは重要です
         // ... その他のPM2設定
