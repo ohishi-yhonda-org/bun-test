8a3e93f41ea5b5433b928f64dc5383e8015ab960
yhonda-ohishi <m.tama.ramu@gmail.com>
2025-07-16 15:22:01 +0900

    f

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,7 +4,7 @@ module.exports = {
         name: 'bun-test-app',
         script: 'dist/index.js',
         // このパスが正しく、絶対パスであることを確認してください
-        cwd: 'C:/actions-runner/_work/bun-***/bun-***/', // <-- これは重要です
+        cwd: 'C:/actions-runner/_work/bun-test/bun-test/', // <-- これは重要です
         // ... その他のPM2設定
     }]
 };
